56353551b07c361b9e7aebd017c0a0c95e63a8d2
Christine Cha <hello@christinecha.com>
2016-03-18 11:35:06 -0400

    Added likes, plays, comments, and read more expansion.

--- src/components/VideoFeature.js
+++ src/components/VideoFeature.js
@@ -4,33 +4,74 @@ import React from 'react'
 import * as helper from '../helpers.js'
 
 export class VideoFeature extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      descriptionExpanded: false,
+      screenWidth: screenWidth
+    }
+  }
+
+  componentWillUpdate() {
+    window.addEventListener("resize", function() {
+      this.setState({
+        screenWidth: window.innerWidth
+      })
+    })
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps != this.props) {
+      this.setState({
+        descriptionExpanded: false
+      })
+    }
+  }
+
   getDescription() {
     return this.props.video.description.replace(/<br \/>/g, '\n')
   }
 
+  toggleDescription() {
+    this.setState({
+      descriptionExpanded: !this.state.descriptionExpanded
+    })
+  }
+
   render() {
     const { changeVideo, video } = this.props
+    const { descriptionExpanded, screenWidth } = this.state
+
     const styles = {
       videoFeature: {
         textAlign: "center",
-        width: "800px",
+        width: screenWidth > 850 ? "800px" : screenWidth + "px",
         margin: "0 auto",
         display: "inline-block"
       },
       descriptionContainer: {
         textAlign: "left",
-        backgroundColor: "rgba(0,0,0,0.5)",
+        backgroundColor: "rgba(0,0,0,0.4)",
         maxWidth: "740px",
         margin: "10px auto",
         padding: "30px",
         color: "white",
+        position: "relative"
       },
       description: {
         marginTop: "10px",
         fontSize: "12px",
-        whiteSpace: "nowrap",
+        maxWidth: descriptionExpanded ? null : "85%",
+        whiteSpace: descriptionExpanded ? "pre-wrap" : "nowrap",
         overflow: "scroll",
-        textOverflow: "ellipsis"
+        textOverflow: "ellipsis",
+      },
+      readMore: {
+        display: video.description.length > 100 ? "block" : "none",
+        position: descriptionExpanded || screenWidth < 820 ? null : "absolute",
+        right: "30px",
+        margin: descriptionExpanded || screenWidth < 820 ? "20px 0" : "-22px 0"
       }
     }
 
@@ -44,14 +85,22 @@ export class VideoFeature extends React.Component {
         <div style={styles.videoFeature}>
           <iframe
             src={helper.getPlayerURL(video.url)}
-            width="800"
-            height="500"
+            width={screenWidth > 820 ? "800" : screenWidth}
+            height={screenWidth > 820 ? "500" : "250"}
             frameBorder="0"
             allowFullScreen>
           </iframe>
           <div style={styles.descriptionContainer}>
             <h3>{video.title}</h3>
             <p style={styles.description}>{this.getDescription()}</p>
+            <button style={styles.readMore} onClick={() => this.toggleDescription()}>
+              { this.state.descriptionExpanded ? "collapse" : "read more" }
+            </button>
+            <div className="stats">
+              <span className="plays">{ video.stats_number_of_plays } <i className="fa fa-play"></i></span>
+              <span className="likes">{ video.stats_number_of_likes } <i className="fa fa-heart"></i></span>
+              <span className="comments">{ video.stats_number_of_comments } <i className="fa fa-comment"></i></span>
+            </div>
           </div>
         </div>
         <button
